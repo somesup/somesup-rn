a4cd38a10f975a71bcd34ce0e2154156a6af60ca
llddang <laliddang@gmail.com>
2025-08-22 02:11:34 +0900

    style:# tailwind 및 css 속성 설정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,28 @@
-// app/_layout.tsx
-import "../global.css"; // CSS 파일 임포트
+import "../global.css";
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <SafeAreaProvider>
+      <StatusBar style="light" backgroundColor="#171717" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: {
+            backgroundColor: "#171717",
+            ...StyleSheet.create({
+              text: {
+                color: "#fafafa", // 기본 텍스트 색상
+              },
+            }).text,
+          },
+        }}
+      >
+        <Stack.Screen name="index" />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Text from "../components/ui/text";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">Welcome to Nativewind!</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 items-center justify-center">
+        <Text className="typography-main-title">Welcome to Somesup RN!</Text>
+        <Text className="typography-body2  mt-2">React Native with NativeWind</Text>
+      </View>
+    </SafeAreaView>
   );
 }
