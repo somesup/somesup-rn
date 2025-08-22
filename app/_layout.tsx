6beffc3d0a39d21abf97807ee027fd3ba0b0f8a7
llddang <laliddang@gmail.com>
2025-08-22 10:50:54 +0900

    feat: 선호도 설정 페이지 구현

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout() {
         <Stack.Screen name="onboarding" />
         <Stack.Screen name="sign-in" />
         <Stack.Screen name="set-nickname" />
+        <Stack.Screen name="set-preferences" />
       </Stack>
       <ToastContainer />
     </SafeAreaProvider>
---
+++ app/set-preferences.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import Text from "@/components/ui/text";
+import SetPreferenceFirstPage from "@/components/features/set-preference/set-preference-first-page";
+import SetPreferenceDetailPage from "@/components/features/set-preference/set-preference-detail-page";
+import SetPreferenceFinishPage from "@/components/features/set-preference/set-preference-finish-page";
+import { useUserStore } from "@/lib/stores/user";
+import { sectionLabels, sectionMappingId } from "@/types/types";
+import { UpdatePreferencesRequestDto } from "@/types/dto";
+import { authUpdatePreferences } from "@/lib/apis/apis";
+import { toast } from "@/components/ui/toast";
+import { SITEMAP } from "@/data/sitemap";
+import SetPreferenceWaitPage from "@/components/features/set-preference/set-preference-wait-page";
+
+const SetPreferencesPage = () => {
+  const [step, setStep] = useState<"first" | "detail" | "finish" | "wait">("first");
+
+  const handlePreferenceDone = async () => {
+    setStep("wait");
+
+    const preferences = useUserStore.getState().preferences;
+    const request = sectionLabels.reduce((acc, label) => {
+      acc.push({ sectionId: sectionMappingId[label], preference: preferences[label] });
+      return acc;
+    }, [] as UpdatePreferencesRequestDto);
+
+    const { error } = await authUpdatePreferences(request);
+    if (!error) return router.push(SITEMAP.HOME);
+    toast.serverError();
+  };
+
+  return (
+    <SafeAreaView className="flex-1">
+      {step !== "wait" && (
+        <View className="px-8 pt-6">
+          <View className="relative">
+            <Text className="text-center typography-small-title">맞춤 설정</Text>
+          </View>
+        </View>
+      )}
+      {step === "first" && <SetPreferenceFirstPage onConfirm={() => setStep("detail")} />}
+      {step === "detail" && <SetPreferenceDetailPage onConfirm={() => setStep("finish")} />}
+      {step === "finish" && (
+        <SetPreferenceFinishPage
+          onReSetup={() => setStep("detail")}
+          onConfirm={handlePreferenceDone}
+        />
+      )}
+      {step === "wait" && <SetPreferenceWaitPage />}
+    </SafeAreaView>
+  );
+};
+
+export default SetPreferencesPage;
